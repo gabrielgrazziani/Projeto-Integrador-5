39dc0d8cfbd26ebd7c6fbe52324b015e53451f88
gabriel_grazziani <gabriel.grazzianibarbosa@gmail.com>
2021-05-02 17:47:13 -0300

    serviço produto

--- backend/PI5/src/main/java/dev/gabrielgrazziani/controller/ServiçoProdutoController.java
+++ backend/PI5/src/main/java/dev/gabrielgrazziani/controller/ServiçoProdutoController.java
@@ -4,12 +4,22 @@ import java.math.BigDecimal;
 import java.util.ArrayList;
 import java.util.List;
 
+import javax.validation.Valid;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 
 import dev.gabrielgrazziani.dto.PessoaResponse;
+import dev.gabrielgrazziani.dto.ServicoProdutoForm;
+import dev.gabrielgrazziani.dto.ServicoProdutoFormAlter;
 import dev.gabrielgrazziani.dto.ServiçoProdutoResponse;
 import dev.gabrielgrazziani.exceptions.MensException;
 import dev.gabrielgrazziani.model.Tipo;
@@ -28,10 +38,10 @@ public class ServiçoProdutoController {
 		@ApiResponse(code = 200,message = "ok",response = PessoaResponse.class),
 		@ApiResponse(code = 400,message = "Input Invalido",response = MensException.class)
 	})
-	@ApiOperation(value = "Busca uma servico produto",notes = "Não precisa estar logado")
+	@ApiOperation(value = "Busca uma serviço produto",notes = "Não precisa estar logado")
 	@GetMapping("/{id}")
 	private ServiçoProdutoResponse buscar(
-			@ApiParam(value = "id servico produto") 
+			@ApiParam(value = "id serviço produto") 
 			@PathVariable long id
 	) {	
 		return ServiçoProdutoResponse.builder()
@@ -48,7 +58,7 @@ public class ServiçoProdutoController {
 		@ApiResponse(code = 200,message = "ok",response = PessoaResponse.class),
 		@ApiResponse(code = 400,message = "Input Invalido",response = MensException.class)
 	})
-	@ApiOperation(value = "Listar servicos produtos",notes = "Não precisa estar logado")
+	@ApiOperation(value = "Listar serviços produtos",notes = "Não precisa estar logado")
 	@GetMapping()
 	private List<ServiçoProdutoResponse> listar() {	
 		List<ServiçoProdutoResponse> serviçoProdutos = new ArrayList<ServiçoProdutoResponse>();
@@ -57,5 +67,53 @@ public class ServiçoProdutoController {
 			serviçoProdutos.add(buscar(i));
 		}
 		return serviçoProdutos;
+	}
+	
+	@ApiResponses({
+		@ApiResponse(code = 201,message = "created",response = PessoaResponse.class),
+		@ApiResponse(code = 400,message = "Input Invalido",response = MensException.class)
+	})
+	@ApiOperation(value = "cria um serviço produto",notes = "Precisa estar logado como um FUNCIONARIO")
+	@PostMapping
+	@ResponseStatus(code = HttpStatus.CREATED)
+	private ServiçoProdutoResponse cria(@Valid @RequestBody ServicoProdutoForm form) {	
+		
+		return ServiçoProdutoResponse.builder()
+				.id(4L)
+				.nome(form.getNome())
+				.tipo(form.getTipo())
+				.valorComercial(form.getValorComercial())
+				.valorCusto(form.getValorCusto())
+				.unidadeMedida(form.getUnidadeMedida())
+				.build();
+	}
+	
+	@ApiResponses({
+		@ApiResponse(code = 200,message = "ok",response = PessoaResponse.class),
+		@ApiResponse(code = 400,message = "Input Invalido",response = MensException.class)
+	})
+	@ApiOperation(value = "altera um serviço produto",notes = "Precisa estar logado como um FUNCIONARIO")
+	@PutMapping
+	private ServiçoProdutoResponse altera(@Valid @RequestBody ServicoProdutoFormAlter form) {	
+		
+		return ServiçoProdutoResponse.builder()
+				.id(form.getId())
+				.nome(form.getNome())
+				.tipo(form.getTipo())
+				.valorComercial(form.getValorComercial())
+				.valorCusto(form.getValorCusto())
+				.unidadeMedida(form.getUnidadeMedida())
+				.build();
+	}
+	
+	@ApiResponses({
+		@ApiResponse(code = 204,message = "No Content",response = PessoaResponse.class),
+		@ApiResponse(code = 400,message = "Input Invalido",response = MensException.class)
+	})
+	@ApiOperation(value = "exclui um serviço produto",notes = "Precisa estar logado como um FUNCIONARIO")
+	@DeleteMapping("/{id}")
+	@ResponseStatus(code = HttpStatus.NO_CONTENT)
+	private void exclui(@ApiParam(value = "id serviço produto") @PathVariable Long id) {	
+		
 	}
 }
---
+++ backend/PI5/src/main/java/dev/gabrielgrazziani/dto/ServicoProdutoForm.java
@@ -0,0 +1,36 @@
+package dev.gabrielgrazziani.dto;
+
+import java.math.BigDecimal;
+
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.Positive;
+import javax.validation.constraints.Size;
+
+import com.sun.istack.NotNull;
+
+import dev.gabrielgrazziani.model.Tipo;
+import io.swagger.annotations.ApiModelProperty;
+import lombok.Data;
+
+@Data
+public class ServicoProdutoForm {
+	
+	@NotBlank
+	@Size(min = 6)
+	private String nome;
+	
+	@NotNull
+	@ApiModelProperty(example = "PRODUTO")
+	private Tipo tipo;
+	
+	@ApiModelProperty(notes = "O valor que este Serviço/Produto custa para a empresa")
+	@Positive
+	private BigDecimal valorCusto;
+	
+	@ApiModelProperty(notes = "O valor que este Serviço/Produto é vendido")
+	@Positive
+	private BigDecimal valorComercial;
+	
+	@NotNull
+	private String unidadeMedida;
+}
