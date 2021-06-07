7c2a61b14b88998dedaf5f90e64d6a046074a971
Celso <celso.iel@sistemafieg.org.br>
2021-06-07 01:42:41 -0300

    fazendo bagunça no projeto

--- frontend/PI5frontend/src/app/app.component.ts
+++ frontend/PI5frontend/src/app/app.component.ts
@@ -1,10 +1,21 @@
-import { Component } from '@angular/core';
+﻿import { Component } from '@angular/core';
+import { Router } from '@angular/router';
 
-@Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
-})
+import { AuthenticationService } from './_services';
+import { User } from './_models';
+
+@Component({ selector: 'app', templateUrl: 'app.component.html' })
 export class AppComponent {
-  title = 'PI5frontend';
+    user: User;
+
+    constructor(
+        private router: Router,
+        private authenticationService: AuthenticationService
+    ) {
+        this.authenticationService.user.subscribe(x => this.user = x);
+    }
+
+    logout() {
+        this.authenticationService.logout();
+    }
 }
